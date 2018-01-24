0ce6b5046d34284051f51219ac77da92df0c5165
PankajSharmaaa <bahman.pankaj@gmail.com>
2018-01-24 11:34:53 +0530

    python

---
+++ string.py
@@ -0,0 +1,10 @@
+Python 3.5.0 (v3.5.0:374f501f4567, Sep 13 2015, 02:16:59) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x='they are not serious'
+>>> x
+'they are not serious'
+>>> x.capitalize()
+'They are not serious'
+>>> x.islower()
+True
+>>> 
