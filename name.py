27aae15723a9b72ef714d8b86daa4a9ec9f75337
PankajSharmaaa <bahman.pankaj@gmail.com>
2018-02-21 11:32:36 +0530

    file update

---
+++ name.py
@@ -0,0 +1,29 @@
+Python 3.5.0 (v3.5.0:374f501f4567, Sep 13 2015, 02:16:59) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> i is in 'manikant'
+SyntaxError: invalid syntax
+>>> 'i' in 'manikant'
+True
+>>> 2 in list
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    2 in list
+TypeError: argument of type 'type' is not iterable
+>>> list =[1,2,3,4,5,6,7,8,9]
+>>> list
+[1, 2, 3, 4, 5, 6, 7, 8, 9]
+>>> list[1:6:1]
+[2, 3, 4, 5, 6]
+>>> list[1:5:2]
+[2, 4]
+>>> i is in 'manikant'
+SyntaxError: invalid syntax
+>>> 'i' in 'manikant'
+True
+>>> 2 in list
+True
+>>> name = input("Are you going to manali ?")
+Are you going to manali ?yes
+>>> name
+'yes'
+>>> 
